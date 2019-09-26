34f727e13a742304e52cc0a5ff4eb22a3e2accb4
Kartik Chauhan <kckartik17@gmail.com>
2019-09-26 14:23:52 +0530

    updated 26 sep

--- 24Sep/main.js
+++ 24Sep/main.js
@@ -3,19 +3,20 @@ var btn = document
   .addEventListener("click", calculate);
 
 function calculate() {
-  var basic, hra, da, ta, gs, tax, net;
-  basic = parseInt(document.getElementById("basic").value);
-  hra = basic * 0.3;
-  da = basic * 0.9;
-  ta = basic * 0.2;
-  gs = basic + hra + da + ta;
-  tax = gs * 0.1;
-  net = gs - tax;
+  salaryOperations.setbasicSalary(parseInt(document.getElementById("basic").value));
+  // var basic, hra, da, ta, gs, tax, net;
+  // basic = parseInt(document.getElementById("basic").value);
+  // hra = basic * 0.3;
+  // da = basic * 0.9;
+  // ta = basic * 0.2;
+  // gs = basic + hra + da + ta;
+  // tax = gs * 0.1;
+  // net = gs - tax;
 
-  document.getElementById("hra").innerText = hra;
-  document.getElementById("da").innerText = da;
-  document.getElementById("ta").innerText = ta;
-  document.getElementById("gs").innerText = gs;
-  document.getElementById("tax").innerText = tax;
-  document.getElementById("net").innerText = net;
+  document.getElementById("hra").innerText = salaryOperations.hra();
+  document.getElementById("da").innerText = salaryOperations.da();
+  document.getElementById("ta").innerText = salaryOperations.ta();
+  document.getElementById("gs").innerText = salaryOperations.gs();
+  document.getElementById("tax").innerText = salaryOperations.tax();
+  document.getElementById("net").innerText = salaryOperations.ns();
 }
